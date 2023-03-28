0ff078e6e2948f8c01dd5095523fe174755e058f
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2023-03-28 02:10:33 +0000

    fix: getTokenIconSrc & getChainIconSrc return absolute URL

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,11 +1,11 @@
 export function getTokenIconSrc(symbol: string) {
-  if (symbol === 'SLP') return './slp.png';
-  if (symbol === 'BUSD') return './busd.png';
-  if (symbol === 'WETH') return './weth.png';
+  if (symbol === 'SLP') return 'https://s2.coinmarketcap.com/static/img/coins/64x64/5824.png';
+  if (symbol === 'BUSD') return 'https://s2.coinmarketcap.com/static/img/coins/64x64/4687.png';
+  if (symbol === 'WETH') return 'https://s2.coinmarketcap.com/static/img/coins/64x64/2396.png';
   return './question-mark.png';
 }
 
 export function getChainIconSrc(id: number) {
-  if (id === 2020) return './ronin.png';
+  if (id === 2020) return 'https://s2.coinmarketcap.com/static/img/coins/64x64/14101.png';
   return './question-mark.png';
 }
